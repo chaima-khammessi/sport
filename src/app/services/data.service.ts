abd51b9d461ed4eba3a720a86f096f07870fad86
chaima khammessi <khammessichaima1@gmail.com>
2020-09-03 11:11:14 +0100

    problem in the attribut

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -15,10 +15,10 @@ export class DataService implements InMemoryDbService{
     { id: 3, name: 'Football League', name2: 'Soccer', image: 'assets/images/logo_1.png',image2: 'assets/images/logo_2.png', date: "19/08/2020",time:'9:30 AM GMT+0',score:'2',score2:'3' },
    ];
    let players= [
-    { id:1,name:'salah', poste:'attaquant',age:'31'},
-    { id:2,name:'mohamed', poste:'attaquant',age:'29'},
-    { id:3,name:'yahya', poste:'attaquant',age:'30'},
-    { id:4,name:'youssef', poste:'attaquant',age:'25'}
+    { id:1,name:'salah', poste:'attaquant',age:'31',image: 'assets/images/img_1.jpg',read: 'Read more',date: "19/08/2020",paragraphe:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus deserunt saepe tempora dolorem.',question:'Romolu to stay at Real Nadrid?'},
+    { id:2,name:'mohamed', poste:'attaquant',age:'29',image: 'assets/images/img_1.jpg',read: 'Read more',date: "19/08/2020",paragraphe:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus deserunt saepe tempora dolorem.',question:'Romolu to stay at Real Nadrid?'},
+    { id:3,name:'yahya', poste:'attaquant',age:'30',image: 'assets/images/img_1.jpg',read: 'Read more',date: "19/08/2020",paragraphe:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus deserunt saepe tempora dolorem.',question:'Romolu to stay at Real Nadrid?'},
+    { id:4,name:'youssef', poste:'attaquant',age:'25',image: 'assets/images/img_1.jpg',read: 'Read more',date: "19/08/2020",paragraphe:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus deserunt saepe tempora dolorem.',question:'Romolu to stay at Real Nadrid?'}
   ];
 
   let users = [
